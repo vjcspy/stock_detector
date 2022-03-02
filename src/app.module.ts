a4e042ea41835a2a9dcd79d5b6e872638f6284d2
Đình Khôi <dinhkhoi.le05@gmail.com>
2022-03-02 16:37:40 +0700

    _ refactor config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FinanInfoModule } from '@module/finan-info/finan-info.module';
 import { CoreModule } from '@module/core/core.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import databaseCfg from '@cfg/database.cfg';
-import rabbitmqCfg from '@cfg/rabbitmq.cfg';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [rabbitmqCfg],
-    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: databaseCfg().host,
--- src/modules/core/core.module.ts
+++ src/modules/core/core.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { appInitAction } from '@module/core/store/actions';
 import { CORE_EFFECTS } from '@module/core/store/effects';
 import { StateManager } from '@module/core/provider/state-manager';
+import { ConfigModule } from '@nestjs/config';
+import rabbitmqCfg from '@cfg/rabbitmq.cfg';
+import databaseCfg from '@cfg/database.cfg';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [rabbitmqCfg, databaseCfg],
+    }),
+  ],
   providers: [StateManager],
   exports: [StateManager],
 })
