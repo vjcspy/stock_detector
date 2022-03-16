93371a04e1ce6f40a98cf009f590ba174f321574
Đình Khôi <dinhkhoi.le05@gmail.com>
2022-03-16 09:55:22 +0700

    _

--- src/config/rabbitmq.cfg.ts
+++ src/config/rabbitmq.cfg.ts
@@ -2,5 +2,5 @@ export default () => ({
   host: process.env.RABBITMQ_HOST || 'localhost',
   port: parseInt(process.env.RABBITMQ_PORT, 10) || 5672,
   user: process.env.RABBITMQ_DEFAULT_USER || 'rabbitmq',
-  pass: process.env.RABBITMQ_DEFAULT_PASS || 'rabbitmq',
+  pass: process.env.RABBITMQ_DEFAULT_PASS || '123456aA@',
 });
