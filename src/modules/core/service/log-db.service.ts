07f321f5d1e8a89fa716fc05877572cd15d3bbd0
Đình Khôi <dinhkhoi.le05@gmail.com>
2022-03-19 22:09:03 +0700

    _

--- src/modules/core/service/log-db.service.ts
+++ src/modules/core/service/log-db.service.ts
@@ -3,13 +3,24 @@ import { InjectModel } from '@nestjs/mongoose';
 import { LogDb, LogDbDocument } from '@module/core/schemas/log-db.schema';
 import { Model } from 'mongoose';
 
+enum Levels {
+  error = 'error',
+  warn = 'warn',
+  info = 'info',
+  http = 'http',
+  verbose = 'verbose',
+  debug = 'debug',
+  silly = 'silly',
+}
+
 export interface LogRecord {
   source: string;
-  message: string;
+  level?: Levels;
   group: string;
   group1?: string;
   group2?: string;
   group3?: string;
+  message: string;
   metadata?: any;
 }
 
