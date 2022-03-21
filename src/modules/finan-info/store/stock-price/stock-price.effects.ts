2b7e23fd09cfeb9669f1d66dcb606d9228725111
Đình Khôi <dinhkhoi.le05@gmail.com>
2022-03-21 11:01:37 +0700

    add log for sync price

--- src/modules/finan-info/store/stock-price/stock-price.effects.ts
+++ src/modules/finan-info/store/stock-price/stock-price.effects.ts
@@ -21,6 +21,7 @@ import { Nack } from '@golevelup/nestjs-rabbitmq';
 import { Injectable } from '@nestjs/common';
 import { Effect } from '@module/core/decorator/store-effect';
 import { LogService } from '@module/core/service/log.service';
+import { Levels } from '@module/core/schemas/log-db.schema';
 
 @Injectable()
 export class StockPriceEffects {
@@ -34,21 +35,51 @@ export class StockPriceEffects {
         return from(getCurrentStatus(action.payload.code)).pipe(
           map((currentStatus) => {
             const curDate = moment();
+            this.log.log({
+              source: 'fi',
+              group: 'sync_price',
+              group1: action.payload.code,
+              message: `Start sync price ${action.payload.code}`,
+            });
             if (currentStatus) {
               const lastDate = moment(currentStatus.lastDate);
               if (lastDate.isSameOrBefore(curDate)) {
+                this.log.log({
+                  source: 'fi',
+                  group: 'sync_price',
+                  group1: action.payload.code,
+                  message: `Lấy dữ liệu từ ngày ${lastDate.format(
+                    'YYYY-MM-DD',
+                  )}`,
+                  metadata: {
+                    lastDate: lastDate.format('YYYY-MM-DD'),
+                    curDate: curDate.format('YYYY-MM-DD'),
+                  },
+                });
                 return getStockPricesAction({
                   code: action.payload.code,
                   lastDate,
                   endDate: curDate,
                 });
               } else {
+                this.log.log({
+                  source: 'fi',
+                  group: 'sync_price',
+                  group1: action.payload.code,
+                  message: `Finish. Đang là dữ liệu mới nhất`,
+                });
                 return stockPricesFinishedAction({
                   code: action.payload.code,
                 });
               }
             } else {
               const lastDate = moment(`${StockPriceValues.START_YEAR}-01-01`);
+              this.log.log({
+                source: 'fi',
+                group: 'sync_price',
+                group1: action.payload.code,
+                message: `Chưa có dữ liệu, lấy từ đầu`,
+              });
               return getStockPricesAction({
                 code: action.payload.code,
                 lastDate,
@@ -75,10 +106,23 @@ export class StockPriceEffects {
         ).pipe(
           map((priceData) => {
             if (priceData === null) {
+              this.log.log({
+                level: Levels.error,
+                source: 'fi',
+                group: 'sync_price',
+                group1: action.payload.code,
+                message: `Error: Không lấy được dữ liệu từ bsc`,
+              });
               return getStockPricesErrorAction({
                 error: new Error('could not get price data'),
               });
             } else {
+              this.log.log({
+                source: 'fi',
+                group: 'sync_price',
+                group1: action.payload.code,
+                message: `Lấy dữ liệu từ BSC thành công`,
+              });
               return getStockPricesAfterAction({
                 data: priceData,
                 code: action.payload.code,
@@ -102,10 +146,23 @@ export class StockPriceEffects {
         ).pipe(
           map((res) => {
             if (res.syncSuccess === true) {
+              this.log.log({
+                source: 'fi',
+                group: 'sync_price',
+                group1: action.payload.code,
+                message: `Save dữ liệu thành công`,
+              });
               return saveStockPriceAfterAction({
                 code: action.payload.code,
               });
             } else {
+              this.log.log({
+                level: Levels.error,
+                source: 'fi',
+                group: 'sync_price',
+                group1: action.payload.code,
+                message: `Error: Save dữ liệu thất bại`,
+              });
               return saveStockPriceErrorAction({
                 error: res.error,
               });
