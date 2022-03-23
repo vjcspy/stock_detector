a75c09a0e6cfd489bc6fff3718c44c6c71be5d43
Đình Khôi <dinhkhoi.le05@gmail.com>
2022-03-23 14:19:13 +0700

    _ create job to sync fi

--- src/modules/finan-info/job/sync-financial-info.job.ts
+++ src/modules/finan-info/job/sync-financial-info.job.ts
@@ -5,6 +5,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CorEntity } from '@module/finan-info/entity/cor.entity';
 import { Repository } from 'typeorm';
 import { Cron } from '@nestjs/schedule';
+import _ from 'lodash';
+import { FinancialInfoValues } from '@module/finan-info/store/financial-info/financial-info.values';
 
 @Injectable()
 export class SyncFinancialInfoJob {
@@ -15,42 +17,72 @@ export class SyncFinancialInfoJob {
     private corRepository: Repository<CorEntity>,
   ) {}
 
-  @Cron('0 10 0 * * *', {
-    name: 'fi_sync_bs_year',
+  /* INDICATOR */
+  @Cron('0 10 22 * * *', {
+    name: 'fi_sync_in_year',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
-  indicatorYear() {
+  async indicatorYear() {
     this.log.log({
       source: 'cron',
       group: 'fi',
       group1: 'sync_in_year',
       message: 'Trigger sync Indicator year',
     });
+    const cors = await this.corRepository.find();
+    _.forEach(cors, (cor) => {
+      this.amqpConnection.publish(
+        FinancialInfoValues.EXCHANGE_KEY,
+        FinancialInfoValues.PUBLISHER_ROUTING_KEY_INDICATOR_YEAR,
+        cor.code,
+        {},
+      );
+    });
   }
 
-  @Cron('0 10 1 * * *', {
-    name: 'fi_sync_bs_quarter',
+  @Cron('0 10 23 * * *', {
+    name: 'fi_sync_in_quarter',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
-  indicatorQuarter() {
+  async indicatorQuarter() {
     this.log.log({
       source: 'cron',
       group: 'fi',
       group1: 'sync_in_quarter',
       message: 'Trigger sync Indicator quarter',
     });
+    const cors = await this.corRepository.find();
+    _.forEach(cors, (cor) => {
+      this.amqpConnection.publish(
+        FinancialInfoValues.EXCHANGE_KEY,
+        FinancialInfoValues.PUBLISHER_ROUTING_KEY_INDICATOR_QUARTER,
+        cor.code,
+        {},
+      );
+    });
   }
 
+  /* BALANCE SHEET */
   @Cron('0 10 0 * * *', {
     name: 'fi_sync_bs_year',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
-  balanceSheetYear() {
+  async balanceSheetYear() {
     this.log.log({
       source: 'cron',
       group: 'fi',
       group1: 'sync_bs_year',
       message: 'Trigger sync Balance Sheet year',
+    });
+
+    const cors = await this.corRepository.find();
+    _.forEach(cors, (cor) => {
+      this.amqpConnection.publish(
+        FinancialInfoValues.EXCHANGE_KEY,
+        FinancialInfoValues.PUBLISHER_ROUTING_KEY_BS_YEAR,
+        cor.code,
+        {},
+      );
     });
   }
 
@@ -58,25 +90,45 @@ export class SyncFinancialInfoJob {
     name: 'fi_sync_bs_quarter',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
-  balanceSheetQuarter() {
+  async balanceSheetQuarter() {
     this.log.log({
       source: 'cron',
       group: 'fi',
       group1: 'sync_bs_quarter',
       message: 'Trigger sync Balance Sheet quarter',
     });
+    const cors = await this.corRepository.find();
+    _.forEach(cors, (cor) => {
+      this.amqpConnection.publish(
+        FinancialInfoValues.EXCHANGE_KEY,
+        FinancialInfoValues.PUBLISHER_ROUTING_KEY_BS_QUARTER,
+        cor.code,
+        {},
+      );
+    });
   }
 
+  /* BUSINESS REPORT */
   @Cron('0 10 2 * * *', {
     name: 'fi_sync_br_year',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
-  businessReportYear() {
+  async businessReportYear() {
     this.log.log({
       source: 'cron',
       group: 'fi',
       group1: 'sync_br_year',
       message: 'Trigger sync Business Report year',
+    });
+
+    const cors = await this.corRepository.find();
+    _.forEach(cors, (cor) => {
+      this.amqpConnection.publish(
+        FinancialInfoValues.EXCHANGE_KEY,
+        FinancialInfoValues.PUBLISHER_ROUTING_KEY_BR_YEAR,
+        cor.code,
+        {},
+      );
     });
   }
 
@@ -84,25 +136,46 @@ export class SyncFinancialInfoJob {
     name: 'fi_sync_br_quarter',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
-  businessReportQuarter() {
+  async businessReportQuarter() {
     this.log.log({
       source: 'cron',
       group: 'fi',
       group1: 'sync_br_quarter',
       message: 'Trigger sync Business Report quarter',
     });
+
+    const cors = await this.corRepository.find();
+    _.forEach(cors, (cor) => {
+      this.amqpConnection.publish(
+        FinancialInfoValues.EXCHANGE_KEY,
+        FinancialInfoValues.PUBLISHER_ROUTING_KEY_BR_QUARTER,
+        cor.code,
+        {},
+      );
+    });
   }
 
+  /* CASH FLOW */
   @Cron('0 10 4 * * *', {
     name: 'fi_sync_cf_year',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
-  cashFlowYear() {
+  async cashFlowYear() {
     this.log.log({
       source: 'cron',
       group: 'fi',
       group1: 'sync_cf_year',
       message: 'Trigger sync Cash Flow year',
+    });
+
+    const cors = await this.corRepository.find();
+    _.forEach(cors, (cor) => {
+      this.amqpConnection.publish(
+        FinancialInfoValues.EXCHANGE_KEY,
+        FinancialInfoValues.PUBLISHER_ROUTING_KEY_CF_YEAR,
+        cor.code,
+        {},
+      );
     });
   }
 
@@ -110,12 +183,22 @@ export class SyncFinancialInfoJob {
     name: 'fi_sync_cf_quarter',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
-  cashFlowQuarter() {
+  async cashFlowQuarter() {
     this.log.log({
       source: 'cron',
       group: 'fi',
       group1: 'sync_cf_quarter',
       message: 'Trigger sync Cash Flow quarter',
+    });
+
+    const cors = await this.corRepository.find();
+    _.forEach(cors, (cor) => {
+      this.amqpConnection.publish(
+        FinancialInfoValues.EXCHANGE_KEY,
+        FinancialInfoValues.PUBLISHER_ROUTING_KEY_CF_QUARTER,
+        cor.code,
+        {},
+      );
     });
   }
 }
