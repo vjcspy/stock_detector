0a93e3825110a2dc9039f188dd1f5fa0b83be184
Đình Khôi <dinhkhoi.le05@gmail.com>
2022-03-25 16:54:10 +0700

    _ refactor

--- python/README.md
+++ python/README.md
@@ -1,4 +1,13 @@
+## Start server
+
 1. python3 -m venv venv/
 2. source venv/bin/activate
 3. pip install -r requirements.txt
-4. python manage.py runserver
+4. python manage.py migrate
+5. python manage.py runserver
+
+## Create admin user
+python manage.py createsuperuser --email admin@example.com --username admin
+
+## After install pip package, run:
+pip freeze > requirements.txt
--- python/main/wsgi.py
+++ python/main/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for stock_computed project.
+WSGI config for main project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock_computed.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = get_wsgi_application()
