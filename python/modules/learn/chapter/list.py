170f17893af0e557f68a509b4082014c33dbba34
Đình Khôi <dinhkhoi.le05@gmail.com>
2022-03-26 10:31:43 +0700

    _ learn

---
+++ python/modules/learn/chapter/list.py
@@ -0,0 +1,10 @@
+#  Trong python gọi là list, nó giống như array. They can contain any type of variable
+myList = []
+myList.append(1)
+myList.append(12)
+myList.append(13)
+
+for x in myList:
+    print(x)
+    
+    
---
+++ python/modules/learn/chapter/variable.py
@@ -0,0 +1,18 @@
+# Python không có kiểu statically typed
+# Every variable in python is an object
+
+# Number. Support 2 types of numbers: integer and floating point
+myInt = 7
+myFloat = 7.0
+
+# simultaneousely declare
+a, b = 3, 4
+print(a,b)
+
+# String
+string1 = "A"
+string2 = "B"
+
+hello = string1 + string2
+
+
