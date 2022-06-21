97134eb9fd7288dbd1bc1bbb9f934e2d4b1dd70a
dinhkhoi <mr.vjcspy@gmail.com>
2022-06-21 22:38:09 +0700

    feat: add slack bolt

--- src/slack.service.ts
+++ src/slack.service.ts
@@ -18,6 +18,10 @@ export class SlackService {
     });
 
     this.boltApp.event('app_mention', this.onAppMention.bind(this));
+    this.boltApp.message('hello', async ({ message, say }) => {
+      // say() sends a message to the channel where the event was triggered
+      await say(`Hey there!`);
+    });
   }
 
   public async onAppMention({ event, client, logger }) {
