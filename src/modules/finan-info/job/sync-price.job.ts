64777c1ba070fdb78565017443075b1b30a5752e
vjcspy <mr.vjcspy@gmail.com>
2022-09-02 22:51:05 +0700

    chore

--- src/modules/finan-info/job/sync-price.job.ts
+++ src/modules/finan-info/job/sync-price.job.ts
@@ -7,32 +7,29 @@ import { SLACK_CHANNEL } from '@cfg/slack.cfg';
 
 @Injectable()
 export class SyncPriceJob {
-  constructor(
-    private syncStockPricePublisher: SyncStockPricePublisher,
-    private log: LogService,
-    private slackService: SlackService,
-  ) {}
-
-  /*
-   * 16h hằng ngày sẽ lấy thêm giá
-   * */
-  @Cron('0 0 16 * * *', {
-    name: 'fi_sync_price',
-    timeZone: 'Asia/Ho_Chi_Minh',
-  })
-  publishSyncPrice() {
-    if (process.env.INSTANCE_ID !== '0') return;
-
-    this.slackService.postMessage(SLACK_CHANNEL.GENERAL_CHIAKI_BOT_CHANNEL, {
-      text: 'Trigger sync price',
-    });
-    this.log.log({
-      source: 'cron',
-      group: 'fi',
-      group1: 'sync_price',
-      message: 'Trigger sync price',
-    });
-
-    this.syncStockPricePublisher.publish();
-  }
+  // constructor(
+  //   private syncStockPricePublisher: SyncStockPricePublisher,
+  //   private log: LogService,
+  //   private slackService: SlackService,
+  // ) {}
+  // /*
+  //  * 16h hằng ngày sẽ lấy thêm giá
+  //  * */
+  // @Cron('0 0 16 * * *', {
+  //   name: 'fi_sync_price',
+  //   timeZone: 'Asia/Ho_Chi_Minh',
+  // })
+  // publishSyncPrice() {
+  //   if (process.env.INSTANCE_ID !== '0') return;
+  //   this.slackService.postMessage(SLACK_CHANNEL.GENERAL_CHIAKI_BOT_CHANNEL, {
+  //     text: 'Trigger sync price',
+  //   });
+  //   this.log.log({
+  //     source: 'cron',
+  //     group: 'fi',
+  //     group1: 'sync_price',
+  //     message: 'Trigger sync price',
+  //   });
+  //   this.syncStockPricePublisher.publish();
+  // }
 }
